6b0c1b29c111a853aa5424d90c00ff33a2c06c8e
AliceNam93 <yeonwood93@gmail.com>
2022-11-15 17:36:38 +0900

    프로덕트 리스트 레이아웃 구현중

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import './ProductList.scss';
 
 function ProductList() {
-  return <p>Hi</p>;
+  return (
+    <div className="productCardWrap">
+      <div className="indivProductWrap">
+        <div className="innerProduct">
+          <img />
+          <p>신제품</p>
+        </div>
+        <div className="innerProduct">
+          <a href="#">영문 이름 쏼라쏼라</a>
+          <p>한글이름 가나다</p>
+        </div>
+        <div className="innerProduct">
+          <p>가격:비싸다 비싸</p>
+        </div>
+        <div className="innerProduct">
+          <button>미리보기</button>
+        </div>
+        <div className="innerProduct" />
+      </div>
+    </div>
+  );
 }
 
 export default ProductList;
