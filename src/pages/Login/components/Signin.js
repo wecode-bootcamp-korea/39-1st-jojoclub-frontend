6a77e1f8a2514f585fd6fc28c420696e8537908e
Jimin Kang <jimin951218@gmail.com>
2022-11-17 16:50:50 +0900

    ADD: signin, signup 기능 구현

--- src/pages/Login/components/Signin.js
+++ src/pages/Login/components/Signin.js
@@ -1,21 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Signin.scss';
 
 export default function Signin() {
+  const signinFetchFunction = e => {
+    signinIsValidate
+      ? navigate('/')
+      : alert('아이디 또는 비밀번호를 확인해 주세요.');
+  };
+
+  // const signinFetchFunction = () => {
+  //   fetch('api주소', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json;charset=utf-8' },
+  //     body: JSON.stringify({ email: emailValue, password: pwValue }),
+  //   })
+  //     .then(response => {
+  //       if (response.status !== 200) {
+  //         throw new Error('error');
+  //       }
+  //       return response.json();
+  //     })
+  //     .catch(err => {
+  //       alert('아이디 또는 비밀번호를 확인해주세요.');
+  //     })
+  //     .then(data => {
+  //       localStorage.setItem('token', data.accessToken);
+  //       navigate('/');
+  //     });
+  // };
+
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    pw: '',
+  });
+
+  const handleUserInfo = event => {
+    const { name, value } = event.target;
+    setUserInfo(prev => ({ ...prev, [name]: value }));
+  };
+
+  const navigate = useNavigate();
+
+  const signinIsValidate =
+    userInfo.email.includes('@') && userInfo.pw.length > 7;
+
   return (
     <div className="existingCustomers">
       <p className="bold">기존 고객</p>
       <div className="inputLoginBox">
         <div className="inputLogin">
-          <input type="text" placeholder="* 이메일 주소" />
+          <input
+            name="email"
+            type="text"
+            placeholder="* 이메일 주소"
+            value={userInfo.email}
+            onChange={handleUserInfo}
+          />
         </div>
         <div className="inputLogin">
-          <input type="password" placeholder="* 비밀번호" />
+          <input
+            name="pw"
+            type="password"
+            placeholder="* 비밀번호"
+            value={userInfo.pw}
+            onChange={handleUserInfo}
+          />
         </div>
       </div>
       <p className="underline">여기를 클릭하세요.</p>
       {/* 비밀번호 힌트를 보시려면 이메일 주소를 입력해 주세요. */}
-      <button className="loginBtn">로그인</button>
+      <button
+        disabled={!signinIsValidate}
+        className="loginBtn"
+        onClick={signinFetchFunction}
+      >
+        로그인
+      </button>
     </div>
   );
 }
--- src/pages/Login/components/Signup.js
+++ src/pages/Login/components/Signup.js
@@ -1,23 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AGREE_INFO, RECEPTION } from '../AgreeInfo';
 import './Signup.scss';
 
 export default function Signup() {
+  const signupFetchFunction = e => {
+    signupIsValidate
+      ? navigate('/')
+      : alert('아이디 또는 비밀번호를 확인해 주세요.');
+  };
+  // const signupFetchFunction = () => {
+  //   fetch('api주소', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json;charset=utf-8' },
+  //     body: JSON.stringify({ name: name, email: emailValue, password: pwValue }),
+  //   })
+  //     .then(response => response.json());
+  //     .then(response => console.log(response));
+  // };
+
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    email: '',
+    pw: '',
+  });
+
+  const handleUserInfo = event => {
+    const { name, value } = event.target;
+
+    setUserInfo(prev => ({ ...prev, [name]: value }));
+  };
+
+  const navigate = useNavigate();
+
+  const signupIsValidate =
+    userInfo.name.length > 0 &&
+    userInfo.email.includes('@') &&
+    userInfo.pw.length > 0;
+
+  const validatePassword = userInfo.pw.search(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/
+  );
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="newCustomer">
       <div className="inputSignupBox">
         <p className="bold">신규 고객</p>
         <div className="inputSignup">
-          <input type="text" placeholder="* 이름" />
+          <input
+            type="text"
+            placeholder="* 이름"
+            name="name"
+            value={userInfo.name}
+            onChange={handleUserInfo}
+          />
         </div>
         <div className="inputSignup">
-          <input type="text" placeholder="* 이메일 주소" />
+          <input
+            type="text"
+            placeholder="* 이메일 주소"
+            name="email"
+            value={userInfo.email}
+            onChange={handleUserInfo}
+          />
         </div>
         <div className="inputSignup">
-          <input type="password" placeholder="* 비밀번호" />
+          <input
+            type={showPassword ? 'text' : 'password'}
+            placeholder="* 비밀번호"
+            name="pw"
+            value={userInfo.pw}
+            onChange={handleUserInfo}
+          />
         </div>
         <label>
-          <input type="checkbox" value="showPw" /> 비밀번호 표시
+          <input type="checkbox" value="showPw" onChange={togglePassword} />
+          비밀번호 표시
         </label>
         <p className="pwCautionMsg">
           영문 대문자, 소문자, 숫자, 특수문자를 모두 조합하여 8자 이상으로
@@ -88,7 +152,13 @@ export default function Signup() {
           <input type="checkbox" /> *가입자 본인은 만 14세 이상입니다. (만 14세
           이상부터 회원가입이 가능합니다)
         </p>
-        <button className="signupBtn">회원 가입</button>
+        <button
+          disabled={!signupIsValidate || validatePassword < 0}
+          className="signupBtn"
+          onClick={signupFetchFunction}
+        >
+          회원 가입
+        </button>
       </div>
     </div>
   );
