11b095cc04bdacaf3bd75b4a778a6388a883cf20
yuju <yuju@iyujuui-MacBookPro.local>
2022-11-18 15:22:17 +0900

    Nav바 구현 완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -90,69 +90,59 @@ function Nav() {
             >
               <div className="scents">
                 <Link to="/productList">
-                  <div>
-                    <img
-                      className="scent"
-                      src="/images/nav/yellow.jpg"
-                      alt="scent"
-                    />
-                    <h2>시트러스</h2>
-                    <p>라임 바질 앤 마다린</p>
-                    <p>그레이프프루트</p>
-                    <p>얼 그레이 앤 큐컴버</p>
-                  </div>
+                  <img
+                    className="scent"
+                    src="/images/nav/yellow.jpg"
+                    alt="scent"
+                  />
+                  <h2>시트러스</h2>
+                  <p>라임 바질 앤 마다린</p>
+                  <p>그레이프프루트</p>
+                  <p>얼 그레이 앤 큐컴버</p>
                 </Link>
                 <Link to="/productList">
-                  <div>
-                    <img
-                      className="scent"
-                      src="/images/nav/orange.jpg"
-                      alt="scent"
-                    />
-                    <h2>프루티</h2>
-                    <p>잉글리쉬 페어 앤 프리지아</p>
-                    <p>블랙베리 앤 베이</p>
-                    <p>넥타린 블로썸 앤 허니</p>
-                  </div>
+                  <img
+                    className="scent"
+                    src="/images/nav/orange.jpg"
+                    alt="scent"
+                  />
+                  <h2>프루티</h2>
+                  <p>잉글리쉬 페어 앤 프리지아</p>
+                  <p>블랙베리 앤 베이</p>
+                  <p>넥타린 블로썸 앤 허니</p>
                 </Link>
                 <Link to="/productList">
-                  <div>
-                    <img
-                      className="scent"
-                      src="/images/nav/purple.jpg"
-                      alt="scent"
-                    />
-                    <h2>라이트 플로랄</h2>
-                    <p>피그 앤 로터스 플라워</p>
-                    <p>와일드 블루벨</p>
-                    <p>레드 로즈</p>
-                  </div>
+                  <img
+                    className="scent"
+                    src="/images/nav/purple.jpg"
+                    alt="scent"
+                  />
+                  <h2>라이트 플로랄</h2>
+                  <p>피그 앤 로터스 플라워</p>
+                  <p>와일드 블루벨</p>
+                  <p>레드 로즈</p>
                 </Link>
                 <Link to="/productList">
-                  <div>
-                    <img
-                      className="scent"
-                      src="/images/nav/pink.jpg"
-                      alt="scent"
-                    />
-                    <h2>플로랄</h2>
-                    <p>피오니 앤 블러쉬 스웨이드</p>
-                    <p>오렌지 블로썸</p>
-                    <p>허니서클 앤 다바나</p>
-                  </div>
+                  <img
+                    className="scent"
+                    src="/images/nav/pink.jpg"
+                    alt="scent"
+                  />
+                  <h2>플로랄</h2>
+                  <p>피오니 앤 블러쉬 스웨이드</p>
+                  <p>오렌지 블로썸</p>
+                  <p>허니서클 앤 다바나</p>
                 </Link>
                 <Link to="/productList">
-                  <div>
-                    <img
-                      className="scent"
-                      src="/images/nav/green.jpg"
-                      alt="scent"
-                    />
-                    <h2>스파이시</h2>
-                    <p>앰버 앤 라벤더</p>
-                    <p>잉글리쉬 오크 앤 헤이즐넛</p>
-                    <p>바닐라 앤 아니스</p>
-                  </div>
+                  <img
+                    className="scent"
+                    src="/images/nav/green.jpg"
+                    alt="scent"
+                  />
+                  <h2>스파이시</h2>
+                  <p>앰버 앤 라벤더</p>
+                  <p>잉글리쉬 오크 앤 헤이즐넛</p>
+                  <p>바닐라 앤 아니스</p>
                 </Link>
               </div>
             </div>
@@ -160,53 +150,41 @@ function Nav() {
           <div className={`dropmenu ${isProductsOpen ? 'active' : 'inactive'}`}>
             <div className="scents">
               <Link to="/productList">
-                <div>
-                  <img
-                    className="scent"
-                    src="/images/nav/perfume.jpg"
-                    alt="scent"
-                  />
-                  <h2>코롱</h2>
-                  <p>코롱</p>
-                  <p>솔리드 센트</p>
-                </div>
+                <img
+                  className="scent"
+                  src="/images/nav/perfume.jpg"
+                  alt="scent"
+                />
+                <h2>코롱</h2>
+                <p>코롱</p>
+                <p>솔리드 센트</p>
               </Link>
               <Link to="/productList">
-                <div>
-                  <img
-                    className="scent"
-                    src="/images/nav/body.jpg"
-                    alt="scent"
-                  />
-                  <h2>배스 앤 샤워</h2>
-                  <p>바디 앤 핸드워시</p>
-                  <p>바디 스크럽</p>
-                  <p>샤워 젤</p>
-                </div>
+                <img className="scent" src="/images/nav/body.jpg" alt="scent" />
+                <h2>배스 앤 샤워</h2>
+                <p>바디 앤 핸드워시</p>
+                <p>바디 스크럽</p>
+                <p>샤워 젤</p>
               </Link>
               <Link to="/productList">
-                <div>
-                  <img
-                    className="scent"
-                    src="/images/nav/bodycare.jpg"
-                    alt="scent"
-                  />
-                  <h2>바디 케어</h2>
-                  <p>바디 크림</p>
-                  <p>바디 앤 핸드 로션</p>
-                  <p>바디 스크럽</p>
-                </div>
+                <img
+                  className="scent"
+                  src="/images/nav/bodycare.jpg"
+                  alt="scent"
+                />
+                <h2>바디 케어</h2>
+                <p>바디 크림</p>
+                <p>바디 앤 핸드 로션</p>
+                <p>바디 스크럽</p>
               </Link>
               <Link to="/productList">
-                <div>
-                  <img
-                    className="scent"
-                    src="/images/nav/facecare.jpg"
-                    alt="scent"
-                  />
-                  <h2>페이스 케어</h2>
-                  <p>립케어</p>
-                </div>
+                <img
+                  className="scent"
+                  src="/images/nav/facecare.jpg"
+                  alt="scent"
+                />
+                <h2>페이스 케어</h2>
+                <p>립케어</p>
               </Link>
             </div>
           </div>
@@ -223,15 +201,13 @@ function Nav() {
             <Link to="/">
               <li className="secondmenu">스토리</li>
             </Link>
-            <Link to="/">
-              <div className="storymenu">
-                <img
-                  className="wecode"
-                  src="/images/nav/wecode.png"
-                  alt="wecode"
-                />
-                <p>완벽한 선물</p>
-              </div>
+            <Link to="/" className="storymenu">
+              <img
+                className="wecode"
+                src="/images/nav/wecode.png"
+                alt="wecode"
+              />
+              <p>완벽한 선물</p>
             </Link>
           </div>
         </div>
