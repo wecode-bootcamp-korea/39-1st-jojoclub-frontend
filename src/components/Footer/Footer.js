9e989d968dd3f9e1a24a9999c48efabfd3372f7e
Jimin Kang <jimin951218@gmail.com>
2022-11-21 15:58:52 +0900

    MODIFY: footer layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,33 +5,18 @@ function Footer() {
   return (
     <div className="footer">
       <div className="listWrap">
-        <div className="customerService">
-          <p className="listName">고객 서비스</p>
-          <ul>
-            <li>자주 묻는 질문</li>
-            <li>고객관리지원팀</li>
-            <li>나의 프로필</li>
-            <li>나의 오더</li>
-            <li>주문 조회</li>
-            <li>교환 및 환불 규정</li>
-            <li>온라인 부티크 쇼핑</li>
-          </ul>
-        </div>
-        <div className="companyIntroduction">
-          <p className="listName">회사 소개</p>
-          <ul>
-            <li>법인 정보</li>
-            <li>커리어</li>
-          </ul>
-        </div>
-        <div className="socialNetwork">
-          <p className="listName">소셜 네트워크</p>
-          <ul>
-            <li>인스타그램</li>
-            <li>페이스북</li>
-            <li>유튜브</li>
-          </ul>
-        </div>
+        {FOOTER_LIST.map(({ id, title, list }) => {
+          return (
+            <div className="listBox" key={id}>
+              <p className="listName">{title}</p>
+              <ul>
+                {list.map(text => {
+                  return <li key={text}>{text}</li>;
+                })}
+              </ul>
+            </div>
+          );
+        })}
       </div>
       <div className="bottomText">
         <p>© We Malone Korea 2022</p>
@@ -43,3 +28,29 @@ function Footer() {
 }
 
 export default Footer;
+
+const FOOTER_LIST = [
+  {
+    id: 1,
+    title: '고객 서비스',
+    list: [
+      '자주 묻는 질문',
+      '고객관리지원팀',
+      '나의 프로필',
+      '나의 오더',
+      '주문 조회',
+      '교환 및 환불 규정',
+      '온라인 부티크 쇼핑',
+    ],
+  },
+  {
+    id: 2,
+    title: '회사 소개',
+    list: ['법인 정보', '커리어'],
+  },
+  {
+    id: 3,
+    title: '소셜 네트워크',
+    list: ['인스타그램', '페이스북', '유튜브'],
+  },
+];
