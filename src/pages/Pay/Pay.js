c12187cf73dc29451c2f9bb1665d239f11e93fe8
yuju <yuju@iyujuui-MacBookPro.local>
2022-11-23 14:30:58 +0900

    수정완료

--- src/pages/Pay/Pay.js
+++ src/pages/Pay/Pay.js
@@ -1,58 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Pay.scss';
 
 function Pay() {
+  //배송지 정보 가져오기
+  const [deliveryInfo, setDeliveryInfo] = useState([]);
+
+  useEffect(() => {
+    fetch('주소', {
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: localStorage.getItem('accessToken'),
+      },
+    })
+      .then(response => response.json())
+      .then(data => setDeliveryInfo(data));
+  }, []);
+
   return (
     <div className="pay">
       <main>
         <img classname="delivery" src="/images/delivery.png" alt="delivery" />
-
         <div className="deliveryinfo">
           <p>배송지 정보</p>
-          *는 필수 입력 정보입니다.
-          <div>
-            <input type="text" />
-          </div>
-          <div className="address">
-            <input type="text" />
-            <button>우편번호</button>
-          </div>
-          <div>
-            <input type="text" />
-          </div>
-          <input type="text" />
+          <p className="needed">*는 필수 입력 정보입니다.</p>
+          <p>*주소</p>
+          <p>{deliveryInfo.address}</p>
           <p>*휴대전화 번호</p>
-          <div className="threeinput">
-            <select>
-              <option selected>선택</option>
-              <option>010</option>
-              <option>011</option>
-              <option>016</option>
-              <option>017</option>
-              <option>018</option>
-              <option>019</option>
-            </select>
-            <input type="text" />
-            <input type="text" />
-          </div>
-          연락처
-          <div className="threeinput">
-            <select>
-              <option selected>선택</option>
-              <option>010</option>
-              <option>011</option>
-              <option>016</option>
-              <option>017</option>
-              <option>018</option>
-              <option>019</option>
-            </select>
-            <input type="text" />
-            <input type="text" />
-          </div>
+          <p>{deliveryInfo.phone}</p>
           <div className="needbox">
+            <p>배송 요청사항</p>
             <input type="text" />
           </div>
-          배송 정보
           <h2>선물 포장</h2>
           <div className="package">
             <input type="radio" name="option" /> 선물 포장 서비스 선택
@@ -74,30 +53,47 @@ function Pay() {
           <p>
             문의 사항이 있으시거나 고객관리지원팀과 상담을 원하실 경우 다음 링크
             consumercare-kr@jomalone.com를 눌러 이메일 문의 서비스로 보내주시면
-            응대를 도와드리겠습니다. 도움이 필요하시면 고객관리지원팀으로 문의
-            바랍니다. 02-6971-3228 라이브 챗 또는 이메일 문의 서비스를
-            이용하시면 보다 신속하게 응대를 도와드리겠습니다. 감사합니다.
+            응대를 도와드리겠습니다.
+            <p>
+              도움이 필요하시면 고객관리지원팀으로 문의 바랍니다. 02-6971-3228
+            </p>
+            <p>
+              라이브 챗 또는 이메일 문의 서비스를 이용하시면 보다 신속하게
+              응대를 도와드리겠습니다.
+            </p>
+            <p>감사합니다.</p>
           </p>
         </aside>
         <aside>
           <h2>주문 요약</h2>
-          <p>장바구니에 담겨진 수량 3</p>
-          <p>합계 ₩</p>
-          <p>배송비</p>
-          <p>총 합계</p>
+          <p>장바구니에 담겨진 수량 </p>
+          <div className="pricetag">
+            <p>
+              <span>합계 </span>
+              <span className="price">₩</span>
+            </p>
+            <p className="deliveryprice">
+              <span>배송비</span>
+              <span className="price"> ₩0</span>
+            </p>
+            <p>
+              <span>총 합계</span>
+              <span className="price"> ₩</span>
+            </p>
+          </div>
         </aside>
         <aside>
           <h2>쿠폰 코드</h2>
           <p>사용 가능한 쿠폰 코드를 입력하세요.</p>
-          <input type="text" />
+          <input className="coupon" type="text" />
           <button>입력</button>
           <p>한 주문당 코드 1회 사용 가능</p>
         </aside>
-        <aside>
+        <aside classroom="delivery">
           <h2>추가 정보</h2>
-          <div>배송 정보 </div>
-          <div>반품 및 교환 정책</div>
-          <div>구매 가능 수량 안내</div>
+          <Link to="/">배송 정보 </Link>
+          <Link to="/">반품 및 교환 정책</Link>
+          <Link to="/">구매 가능 수량 안내</Link>
         </aside>
       </div>
     </div>
