c7c62eeb2eb9e06351c904064f7571351c936c87
yuju <yuju@iyujuui-MacBookPro.local>
2022-11-23 11:24:11 +0900

    ADD: 장바구니 벡엔드 연결전 완료

--- src/pages/Shopping/Shopping.js
+++ src/pages/Shopping/Shopping.js
@@ -22,6 +22,15 @@ function Shopping() {
     return total;
   };
 
+  //상품 총 개수
+  let totalqty = () => {
+    let total = 0;
+    for (let i = 0; i < itemInfo.length; i++) {
+      total += itemInfo[i].qty;
+    }
+    return total;
+  };
+
   //추천 상품 장바구니 담기
   const addItem = index => {
     setItemInfo([recommenditem[index], ...itemInfo]);
@@ -73,11 +82,33 @@ function Shopping() {
     },
   ];
 
+  //장바구니 추가
+
+  //장바구니 상품 제거
+  const [removeItem, setRemoveItem] = useState('');
+
+  useEffect(() => {
+    fetch('API 주소', {
+      method: 'DELETE',
+      headers: {
+        'content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        productId: '1',
+        sizeId: '10',
+      }),
+    })
+      .then(response => response.json())
+      .then(data => setRemoveItem(data));
+  }, []);
+
   return (
     <div className="shopping">
       <main>
         <h1 className="title">장바구니</h1>
-        <h2 className="subtitle">(3 개의 제품/장바구니에 담긴 제품 수량)</h2>
+        <h2 className="subtitle">
+          ({totalqty() + 1} 개의 제품/장바구니에 담긴 제품 수량)
+        </h2>
 
         <div className="listitems">
           <div>제품</div>
@@ -109,7 +140,7 @@ function Shopping() {
           <Link to="/" className="continueshop">
             쇼핑 계속하기
           </Link>
-          <Link to="/">
+          <Link to="/pay">
             <button className="buybutton">결제하기</button>
           </Link>
         </div>
@@ -138,17 +169,34 @@ function Shopping() {
           <p>
             문의 사항이 있으시거나 고객관리지원팀과 상담을 원하실 경우 다음 링크
             consumercare-kr@jomalone.com를 눌러 이메일 문의 서비스로 보내주시면
-            응대를 도와드리겠습니다. 도움이 필요하시면 고객관리지원팀으로 문의
-            바랍니다. 02-6971-3228 라이브 챗 또는 이메일 문의 서비스를
-            이용하시면 보다 신속하게 응대를 도와드리겠습니다. 감사합니다.
+            응대를 도와드리겠습니다.
+            <p>
+              도움이 필요하시면 고객관리지원팀으로 문의 바랍니다. 02-6971-3228
+            </p>
+            <p>
+              라이브 챗 또는 이메일 문의 서비스를 이용하시면 보다 신속하게
+              응대를 도와드리겠습니다.
+            </p>
+            <p>감사합니다.</p>
           </p>
         </aside>
         <aside>
           <h2>주문 요약</h2>
-          <p>장바구니에 담겨진 수량 3</p>
-          <p>합계 ₩{totalprice()}</p>
-          <p>배송비</p>
-          <p>총 합계</p>
+          <p>장바구니에 담겨진 수량 {totalqty() + 1}</p>
+          <div className="pricetag">
+            <p>
+              <span>합계 </span>
+              <span className="price">₩{totalprice()}</span>
+            </p>
+            <p className="deliveryprice">
+              <span>배송비</span>
+              <span className="price"> ₩0</span>
+            </p>
+            <p>
+              <span>총 합계</span>
+              <span className="price"> ₩{totalprice()}</span>
+            </p>
+          </div>
         </aside>
         <aside>
           <h2>쿠폰 코드</h2>
@@ -157,11 +205,11 @@ function Shopping() {
           <button>입력</button>
           <p>한 주문당 코드 1회 사용 가능</p>
         </aside>
-        <aside>
+        <aside classroom="delivery">
           <h2>추가 정보</h2>
-          <div>배송 정보 </div>
-          <div>반품 및 교환 정책</div>
-          <div>구매 가능 수량 안내</div>
+          <Link to="/">배송 정보 </Link>
+          <Link to="/">반품 및 교환 정책</Link>
+          <Link to="/">구매 가능 수량 안내</Link>
         </aside>
       </div>
     </div>
